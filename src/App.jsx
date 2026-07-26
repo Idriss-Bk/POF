c7751b50aca150976d959eae5efaaaba2faf86db
IDRISS BOUKDIR <150384920+Idriss-Bk@users.noreply.github.com>
2026-07-26 15:08:18 +0400

    add splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
 import Hero from './sections/Hero'
+import SplashScreen from './components/layouts/SplashScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <SplashScreen/>
     <Header/>
     <Hero/>
     <Footer/>
